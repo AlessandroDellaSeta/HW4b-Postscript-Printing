846bfe69847599e16ab48368b50ed7d3c3944630
Shen J <35711676+jsxcool@users.noreply.github.com>
2018-03-26 15:48:22 -0400

    update class

--- main.cpp
+++ main.cpp
@@ -1,11 +1,39 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
-//test//
 class Postscript {
+private:
+    ofstream file;
 public:
+    Postscript(const string& name){
+        file.open (name);    //equals ofstream file(name)
+    }
 
-};
+    void line(int x0, int y0, int x1, int y1){
+        file << x0<<' '<<y0<<" moveto\n"<<x1<<' '<<y1<<" lineto\n"<<"stroke\n";
+        file<<endl;
+    }
+
+    void setColor(float r, float g, float b){
+        file << "setrgbcolor "<< r <<' '<<g <<' '<<b<<"\n";
+        file<<endl;
+    }
+
+    void drawRect(int x, int y, int w, int h){
+        file <<x<<' '<<y<< " moveto\n"<<x+w<<' '<< y<<" lineto\n"<<x+w
+             <<' '<<y+h << " lineto\n"<<x<<' '<< y+h <<" lineto\n"
+             <<"closepath stroke\n";
+        file<<endl;
+    }
+
+    void fillRect(int x, int y, int w, int h){
+        file<<x<<' '<<y<<" moveto\n"<<x+w<<' '<< y<<" lineto\n"<<x+w
+            <<' '<<y+h << " lineto\n"<<x<<' '<< y+h <<" lineto\n"
+            <<"closepath fill\n";
+        file<<endl;
+    }
+
 
 int main() {
     Postscript p("test.ps");
